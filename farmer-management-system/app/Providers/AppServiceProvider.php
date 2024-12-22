40488e9adc6b5b5e6c2d325950d1acb02e69b105
Mafumu12 <mafumumumba12112@gmail.com>
2024-12-22 13:35:44 +0200

    Dynamic registering of service provider

--- farmer-management-system/app/Providers/AppServiceProvider.php
+++ farmer-management-system/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Nwidart\Modules\Facades\Module;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        $modules = Module::all();
+
+        foreach ($modules as $module) {
+            if ($module->isEnabled()) {
+                $serviceProvider = $module->getPath() . '/Providers/' . $module->getName() . 'ServiceProvider.php';
+                if (file_exists($serviceProvider)) {
+                    $this->app->register("Modules\\" . $module->getName() . "\\Providers\\" . $module->getName() . "ServiceProvider");
+                }
+            }
+        }
     }
 }
